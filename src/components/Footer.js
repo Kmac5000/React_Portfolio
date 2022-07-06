ad5d24b9fcc2438030e9a0991126606784570e0d
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-05 18:41:48 -0600

    projects formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,32 @@ import "./footer.css";
 function Footer() {
   return (
     <footer className="footer" id="footerText">
-      Dog Bark, Cat Meow
+      <div className="content has-text-centered">
+        <a
+          className="footText text-left"
+          href="https://github.com/Kmac5000"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github : Kmac5000 ---
+        </a>
+        <a
+          className="footText justify-content-end"
+          href="https://www.linkedin.com/in/kmac929/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn ---
+        </a>
+        <a
+          className="footText"
+          href="https://www.instagram.com/riverbat1/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+      </div>
     </footer>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -41,14 +41,14 @@ function Projects() {
       <Container id="projects-area">
         <Row className="mb-5 mt-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <h1 className="display-4 mb-4"> My Projects </h1>{" "}
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
-        <div className="mb-5 po_items_ho">
+        <div className="mb-5 projectItemsBrowse">
           {portfolioImages.map((data, i) => {
             return (
-              <div key={i} className="po_item">
+              <div key={i} className="projectItem">
                 <img src={data.img} alt="" />
                 <div className="content">
                   <p>{data.desctiption}</p>
