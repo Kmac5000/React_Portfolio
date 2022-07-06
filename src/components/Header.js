935c3cc021a13b66dab2d5ada1c6ac654e99c5f0
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-06 17:32:40 -0600

    header and footer text formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,27 +23,27 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button> */}
-        {/* <div className="collapse navbar-collapse" id="navbarText"> */}
-        <div className="justify-content-end" id="navbarText">
-          <ul className="navbar-nav mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/about" className="nav-link" aria-current="page">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/projects" className="nav-link">
-                Projects
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/contact" className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
+        <div className="container navbarText" id="navbarText">
+          <div className="row-" id="navbarText">
+            <ul className="navbar-nav mb-2 mb-lg-0 justify-content-end">
+              <li className="nav-item">
+                <Link to="/about" className="nav-link" aria-current="page">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/projects" className="nav-link">
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        {/* </div> */}
       </nav>
     </header>
   );
