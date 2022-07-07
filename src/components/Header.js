4519b542fc08a1c31c3be2c55989ba80124ba5e6
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-07 16:38:02 -0600

    added stack, rows, and columns

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,22 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { Stack, Row, Col } from "react-bootstrap";
 
 import "./Header.css";
 
 function Header() {
   return (
-    <header className="navbar navbar-expand-lg navbar-light bg-light">
-      <nav className="container-fluid">
-        <h1 className="navbar-text">
-          <a href="/" alt="home" id="navName">
-            Keith MacNeil
-          </a>
-        </h1>
-        {/* <button
+    <Stack>
+      <Row className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="container-fluid">
+          <Col>
+            <h1 className="navbar-text">
+              <a href="/" alt="home" id="navName">
+                Keith MacNeil
+              </a>
+            </h1>
+          </Col>
+          {/* <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -23,34 +27,35 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button> */}
-        <div className="container navbarText" id="navbarText">
-          <div className="row-" id="navbarText">
-            <ul className="navbar-nav mb-2 mb-lg-0 justify-content-end">
-              <li className="nav-item">
-                <Link to="/about" className="nav-link" aria-current="page">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/projects" className="nav-link">
-                  Projects
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contact" className="nav-link">
-                  Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/resume" className="nav-link">
-                  Resume
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+          <Col className="container navbarText" id="navbarText">
+            <div className="row-" id="navbarText">
+              <ul className="navbar-nav mb-2 mb-lg-0 justify-content-end">
+                <li className="nav-item ms-auto">
+                  <Link to="/about" className="nav-link" aria-current="page">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item ms-auto">
+                  <Link to="/projects" className="nav-link">
+                    Projects
+                  </Link>
+                </li>
+                <li className="nav-item ms-auto">
+                  <Link to="/contact" className="nav-link">
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item ms-auto">
+                  <Link to="/resume" className="nav-link">
+                    Resume
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </nav>
+      </Row>
+    </Stack>
   );
 }
 
