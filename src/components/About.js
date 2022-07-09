355013986520a984c5c73f6516a62779d7592f20
Kmac5000 <102488109+Kmac5000@users.noreply.github.com>
2022-07-08 19:26:26 -0600

    about bio

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,25 @@ import "../../src/App.css";
 function About() {
   return (
     <section className="App-header" id="about-area">
-      <img src="face.png" className="App-logo" alt="face" />
-      <p>A little bit about me</p>
+      <img src="face.png" className="App-logo my-5" alt="face" />
+      <h1 className="bio-header ">A little bit about me</h1>
+      <hr />
+      <p>
+        I was born in hospital in Florida, But I was moved to Colorado at the
+        age of 2.
+      </p>
+      <p>I have an extensive history in Art and Truck Driving.</p>
+      <p>
+        Since I have begun coding I have realized I really love the Front End
+        Design side.
+      </p>
+      <p>
+        I can help a company improve the look feel and functionality of their
+        applications.
+      </p>
+      <p>
+        I am great with react, and css, and have lots of experience with design.
+      </p>
     </section>
   );
 }
